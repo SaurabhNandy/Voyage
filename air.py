0f07f7bc0e20978f38446f70cd0ebdb3b6e91e39
SaurabhNandy <saurabh.nandy@somaiya.edu>
2019-08-07 17:43:19 +0530

    change colors

--- air.py
+++ air.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/aviair/home',methods = ['POST', 'GET'])
 def home():
