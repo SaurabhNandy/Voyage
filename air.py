6de517f3c1cc812fb1f3a853f4c1dbffeed4f3a4
SaurabhNandy <saurabh.nandy@somaiya.edu>
2019-08-08 19:01:41 +0530

    Adding contact page

--- air.py
+++ air.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+
 @app.route('/aviair/home',methods = ['POST', 'GET'])
 def home():
 	airports=['Agartala (IXA)', 'Ahmedabad (AMD)','Prayagraj (IXD)', 'Amritsar (ATQ)', 'Bagdogra (IXB)','Bengaluru (BLR)', 'Bhopal (BHO)', 'Bhubaneswar (BBI)','Chandigarh (IXC)','Chennai (MAA)', 'Coimbatore (CJB)', 'Dehradun (DED)', 'Delhi (DEL)','Dibrugarh (DIB)', 'Dimapur (DMU)', 'Goa (GOI)','Gorakhpur (GOP)', 'Guwahati (GAU)','Hubli (HBX)', 'Hyderabad (HYD)', 'Imphal (IMF)', 'Indore (IDR)','Jabalpur (JLR)', 'Jaipur (JAI)', 'Jammu (IXJ)','Jorhat (JRH)', 'Kannur (CNN)', 'Kochi (COK)','Kolkata (CCU)', 'Kozhikode (CCJ)','Lucknow (LKO)', 'Madurai (IXM)','Mangalore (IXE)', 'Mumbai (BOM)', 'Nagpur (NAG)','Patna (PAT)', 'Port Blair (IXZ)','Pune (PNQ)', 'Raipur (RPR)', 'Rajahmundry (RJA)','Ranchi (IXR)', 'Srinagar (SXR)', 'Surat (STV)','Thiruvananthapuram (TRV)', 'Tiruchirappalli (TRZ)','Tirupati (TIR)',  'Udaipur (UDR)','Vadodara (BDQ)', 'Varanasi (VNS)','Vijayawada (VGA)', 'Visakhapatnam (VTZ)' ]
@@ -10,6 +11,11 @@ def home():
 		return render_template('home.html',airports=airports)
 	else:
 		return render_template('home.html',home='active',airports=airports)
+
+
+@app.route('/aviair/contact',methods = ['POST', 'GET'])
+def contact():
+	return render_template('contact.html',contact='active')
 
 if __name__ == '__main__':
    app.run(debug=True)
